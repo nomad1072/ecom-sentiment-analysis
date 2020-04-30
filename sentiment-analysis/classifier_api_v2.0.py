7d6e595c3d15e690c9f0179fdb74e8460266adcd
nomad1072 <siddharthlanka@gmail.com>
2020-04-29 19:16:44 -0600

    :sparkles: change ML wrapper port

--- sentiment-analysis/classifier_api_v2.0.py
+++ sentiment-analysis/classifier_api_v2.0.py
@@ -56,4 +56,4 @@ def senitmentAnalysis():
     return json.dumps(pred_data)
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port='5002')
+    app.run(debug=False, host='0.0.0.0', port='5000')
