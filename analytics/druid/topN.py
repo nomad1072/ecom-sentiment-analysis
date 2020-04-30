1dd559577815f4061ad1502ff332939ec33aaae1
nomad1072 <siddharthlanka@gmail.com>
2020-04-29 19:16:25 -0600

    :sparkles: change topN druid query

--- analytics/druid/topN.py
+++ analytics/druid/topN.py
@@ -4,69 +4,55 @@ TopN products by events
 topN_query_string = """
     
     {
-        "queryType": "groupBy",
+        "queryType": "topN",
         "dataSource": {
             "type": "table",
             "name": "flink-output-1"
         },
-        "intervals": {
-            "type": "intervals",
-            "intervals": [
-            "2020-04-27T08:18:46.000Z/2020-04-30T15:36:27.903Z"
-            ]
-        },
         "virtualColumns": [],
-        "filter": {
-            "type": "not",
-            "field": {
-            "type": "selector",
-            "dimension": "event",
-            "value": null,
-            "extractionFn": null
-            }
-        },
-        "granularity": {
-            "type": "all"
-        },
-        "dimensions": [
-            {
+        "dimension": {
             "type": "default",
             "dimension": "product_name",
             "outputName": "product_name",
             "outputType": "STRING"
-            },
-            {
-            "type": "default",
-            "dimension": "product_catalog",
-            "outputName": "product_catalog",
-            "outputType": "STRING"
-            }
-        ],
+        },
+        "metric": {
+            "type": "numeric",
+            "metric": "sentiment_sum"
+        },
+        "threshold": 100,
+        "intervals": {
+            "type": "intervals",
+            "intervals": [
+            "2020-04-27T01:10:36.000Z/146140482-04-24T15:36:27.903Z"
+            ]
+        },
+        "filter": {
+            "type": "selector",
+            "dimension": "event",
+            "value": "product_review"
+        },
+        "granularity": {
+            "type": "all"
+        },
         "aggregations": [
             {
             "type": "count",
             "name": "count"
+            },
+            {
+            "type": "doubleSum",
+            "name": "sentiment_sum",
+            "fieldName": "average_sentiment",
+            "expression": null
             }
         ],
         "postAggregations": [],
-        "having": null,
-        "limitSpec": {
-            "type": "default",
-            "columns": [
-            {
-                "dimension": "count",
-                "direction": "descending",
-                "dimensionOrder": {
-                "type": "numeric"
-                }
-            }
-            ],
-            "limit": 10
-        },
         "context": {
             "sqlOuterLimit": 100,
-            "sqlQueryId": "fe53f514-c8f5-4d01-929b-9733e0f6af3d"
+            "sqlQueryId": "1c83447c-cfe3-4756-b033-45565252fc31"
         },
         "descending": false
     }
+    
 """
